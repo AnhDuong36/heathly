d188a6b88bdc58a3e48b4cd0e419d00ba202303d
duonghoangyen <haiiyenn210@gmail.com>
2021-09-22 22:54:09 +0700

    admin

---
+++ Laravel/resources/views/components/admin-aside.blade.php
@@ -0,0 +1,59 @@
+
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+            <!-- Sidebar -->
+            <div class="sidebar">
+                <!-- Sidebar user (optional) -->
+                <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                    <div class="image">
+                        <img src="{{url('public/admin')}}/dist/img/admin.jpg" class="img-circle elevation-2" alt="User Image">
+                    </div>
+                    <div class="info">
+                        <a href="#" class="d-block">Admin</a>
+                        <a href=""><i class="fas fa-sign-out-alt"></i>Đăng Xuất</a>
+                    </div>
+                </div>
+
+                <!-- Sidebar Menu -->
+                <nav class="mt-2">
+                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                        
+                    <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-chart-pie"></i>
+                                <p>
+                                    Danh Mục
+                                    <i class="right fas fa-angle-left"></i>
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Danh sách danh mục</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Thêm mới danh mục</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="" class="nav-link">
+                                        <i class="fas fa-trash nav-icon"></i>
+                                        <p>Thùng rác</p>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                       
+                     
+                      
+                       
+                    </ul>
+
+                </nav>
+                <!-- /.sidebar-menu -->
+            </div>
+            <!-- /.sidebar -->
+        </aside>
--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AdminController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +15,112 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::group(['prefix' => 'admin'], function(){
+    Route::get('/', [AdminController::class,'index'])->name('index');
+
+    // Route::group(['prefix' => 'category'], function(){
+    //     Route::get('/', [CategoryController::class,'index'])->name('category.index');
+    //     Route::get('create', [CategoryController::class,'create'])->name('category.create');
+    //     Route::post('add', [CategoryController::class,'add'])->name('category.add');
+
+    //     Route::get('trashed', [CategoryController::class,'trashed'])->name('category.trashed');
+    //     Route::get('restore/{id}', [CategoryController::class,'restore'])->name('category.restore');
+    //     Route::get('force-delete/{id}', [CategoryController::class,'forceDelete'])->name('category.forceDelete');
+
+    //     Route::get('edit/{cat}', [CategoryController::class,'edit'])->name('category.edit');
+    //     Route::put('update/{cat}', [CategoryController::class,'update'])->name('category.update');
+    //     Route::delete('destroy/{cat}', [CategoryController::class,'destroy'])->name('category.destroy');
+
+    //     Route::delete('deleteAll', [CategoryController::class,'deleteAll'])->name('category.deleteAll');
+
+    //     Route::delete('clear', [CategoryController::class,'clear'])->name('category.clear');
+    // });
+    // Route::group(['prefix' => 'product'], function(){
+    //     Route::get('/', [ProductController::class,'index'])->name('product.index');
+    //     Route::get('create', [ProductController::class,'create'])->name('product.create');
+    //     Route::post('add', [ProductController::class,'add'])->name('product.add');
+
+    //     Route::get('trashed', [ProductController::class,'trashed'])->name('product.trashed');
+    //     Route::get('restore/{id}', [ProductController::class,'restore'])->name('product.restore');
+    //     Route::get('force-delete/{id}', [ProductController::class,'forceDelete'])->name('product.forceDelete');
+
+    //     Route::get('edit/{pro}', [ProductController::class,'edit'])->name('product.edit');
+    //     Route::put('update/{pro}', [ProductController::class,'update'])->name('product.update');
+
+    //     Route::delete('destroy/{pro}', [ProductController::class,'destroy'])->name('product.destroy');
+    //     Route::delete('clear', [ProductController::class,'clear'])->name('product.clear');
+
+    //     Route::delete('deleteAll', [ProductController::class,'deleteAll'])->name('product.deleteAll');
+
+    //     Route::get('retrieve', [ProductController::class,'retrieve'])->name('product.retrieve');
+
+    //     Route::get('image-edit/{image}', [ImageController::class,'edit'])->name('product.image-edit');
+    //     Route::put('image-update/{image}', [ImageController::class,'update'])->name('product.image-update');
+    //     Route::get('image-destroy/{image}', [ImageController::class,'destroy'])->name('product.image-destroy');
+
+    // });
+    // Route::group(['prefix' => 'customer'], function(){
+    //     Route::get('/', [CustomerController::class,'index'])->name('customer.index');
+    //     Route::get('create', [CustomerController::class,'create'])->name('customer.create');
+    //     Route::post('add', [CustomerController::class,'add'])->name('customer.add');
+
+    //     Route::get('trashed', [CustomerController::class,'trashed'])->name('customer.trashed');
+    //     Route::get('restore/{id}', [CustomerController::class,'restore'])->name('customer.restore');
+    //     Route::get('force-delete/{id}', [CustomerController::class,'forceDelete'])->name('customer.forceDelete');
+
+    //     Route::get('edit/{cus}', [CustomerController::class,'edit'])->name('customer.edit');
+    //     Route::put('update/{cus}', [CustomerController::class,'update'])->name('customer.update');
+
+    //     Route::delete('destroy/{cus}', [CustomerController::class,'destroy'])->name('customer.destroy');
+    //     Route::delete('clear', [CustomerController::class,'clear'])->name('customer.clear');
+    //     Route::get('retrieve', [CustomerController::class,'retrieve'])->name('customer.retrieve');
+    //     Route::delete('deleteAll', [CustomerController::class,'deleteAll'])->name('customer.deleteAll');
+    // });
+    // Route::group(['prefix' => 'banner'], function(){
+    //     Route::get('/', [BannerController::class,'index'])->name('banner.index');
+    //     Route::get('create', [BannerController::class,'create'])->name('banner.create');
+    //     Route::post('add', [BannerController::class,'add'])->name('banner.add');
+
+    //     Route::get('edit/{ban}', [BannerController::class,'edit'])->name('banner.edit');
+    //     Route::put('update/{ban}', [BannerController::class,'update'])->name('banner.update');
+
+    //     Route::delete('destroy/{ban}', [BannerController::class,'destroy'])->name('banner.destroy');
+
+    // });
+    // Route::group(['prefix' => 'advertisement'], function(){
+    //     Route::get('/', [AdvertisementController::class,'index'])->name('advertisement.index');
+    //     Route::get('edit/{adv}', [AdvertisementController::class,'edit'])->name('advertisement.edit');
+    //     Route::put('update/{adv}', [AdvertisementController::class,'update'])->name('advertisement.update');
+
+    // });
+    // Route::group(['prefix' => 'comment'], function(){
+    //     Route::get('/', [CommentController::class,'index'])->name('comment.index');
+    //     Route::get('create', [CommentController::class,'create'])->name('comment.create');
+    //     Route::post('add', [CommentController::class,'add'])->name('comment.add');
+
+    //     Route::get('edit/{com}', [CommentController::class,'edit'])->name('comment.edit');
+    //     Route::put('update/{com}', [CommentController::class,'update'])->name('comment.update');
+
+    //     Route::delete('destroy/{com}', [CommentController::class,'destroy'])->name('comment.destroy');
+    //     Route::delete('clear', [CommentController::class,'clear'])->name('comment.clear');
+    // });
+    // Route::group(['prefix' => 'admin'], function(){
+    //     Route::get('/', [AdminsController::class,'index'])->name('admin.index');
+    //     Route::get('create', [AdminsController::class,'create'])->name('admin.create');
+    //     Route::post('add', [AdminsController::class,'add'])->name('admin.add');
+    //     Route::get('edit/{adm}', [AdminsController ::class,'edit'])->name('admin.edit');
+    //     Route::put('update/{adm}', [AdminsController ::class,'update'])->name('admin.update');
+    //     Route::delete('destroy/{adm}', [AdminsController::class,'destroy'])->name('admin.destroy');
+
+    // });
+    // Route::group(['prefix' => 'order'], function(){
+    //     Route::get('/', [OrderController::class,'index'])->name('order.index');
+    //     Route::get('order/detail/{id}', [OrderController::class,'detail'])->name('admin.order.detail');
+    //     Route::put('order/status/{id}', [OrderController::class,'status'])->name('order.status');
+
+
+    // });
+
+
 });
